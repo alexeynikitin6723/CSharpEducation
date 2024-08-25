7b6073830c426ddcde678155f6087cd952c360b9
Alexey_Nikitin <alexey.nikitin6723@gmail.com>
2024-08-25 21:16:41 +0400

    add task1

--- Task1/obj/Debug/net6.0/Task1.AssemblyInfo.cs
+++ Task1/obj/Debug/net6.0/Task1.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("TicTacToe")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Task1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97678c5434a54f2b89f0e443a9d6b77b7bc35410")]
-[assembly: System.Reflection.AssemblyProductAttribute("TicTacToe")]
-[assembly: System.Reflection.AssemblyTitleAttribute("TicTacToe")]
+[assembly: System.Reflection.AssemblyProductAttribute("Task1")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Task1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
