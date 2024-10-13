81b26dde995983b304f9bd747e694ba1717f63f3
Alexey_Nikitin <alexey.nikitin6723@gmail.com>
2024-10-13 20:54:35 +0400

    Добавлена домашка Исключения

---
+++ UserManagement/Program.cs
@@ -0,0 +1,120 @@
+﻿namespace UserManagement
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      UserManager userManager = new UserManager();
+      Console.WriteLine("1. Добавить пользователя");
+      Console.WriteLine("2. Удалить пользователя");
+      Console.WriteLine("3. Список пользователей");
+      Console.WriteLine("4. Выход");
+
+      while (true)
+      {
+        Console.Write("Введите команду: ");
+        string command = Console.ReadLine();
+
+        try
+        {
+          switch (command)
+          {
+            case "1":
+              AddUser(userManager);
+              break;
+
+            case "2":
+              RemoveUser(userManager);
+              break;
+
+            case "3":
+              ListUsers(userManager);
+              break;
+
+            case "4":
+              return;
+
+            default:
+              Console.WriteLine("Неверный выбор, попробуйте снова.");
+              break;
+          }
+        }
+        catch (Exception ex)
+        {
+          Console.WriteLine($"Ошибка: {ex.Message}");
+        }
+      }
+    }
+    
+    static void AddUser(UserManager userManager)
+    {
+      try
+      {
+        Console.Write("Введите Имя: ");
+        string name = Console.ReadLine();
+
+        Console.Write("Введите возраст: ");
+        int age = int.Parse(Console.ReadLine());
+
+        User user = new User(name, age);
+        userManager.AddUser(user);
+        Console.WriteLine("Пользователь добавлен");
+      }
+      catch (FormatException)
+      {
+        Console.WriteLine("Неверный формат ввода");
+      }
+      catch(Exception ex)
+      {
+        Console.WriteLine($"Ошибка: {ex.Message}");
+      }
+    }
+    static void RemoveUser(UserManager userManager)
+    {
+      try
+      {
+        Console.Write("Введите имя пользователя, которого нужно удалить: ");
+        string name = Console.ReadLine();
+        if (userManager.RemoveUser(name))
+        {
+          Console.WriteLine("Пользователь удален.");
+        } 
+        else
+        {
+          Console.WriteLine("Пользователь не найден");
+        }
+      }
+      catch (FormatException)
+      {
+        Console.WriteLine("Неверный формат ввода");
+      }
+      catch (Exception ex)
+      {
+        Console.WriteLine($"Ошибка: {ex.Message}");
+      }
+    }
+
+    static void ListUsers(UserManager userManager)
+    {
+      try
+      {
+        var users = userManager.ListUsers();
+        if (users == null)
+        {
+          throw new InvalidOperationException("Списка пользователей нет");
+        }
+        else if(users.Count == 0)
+        {
+          Console.WriteLine("Список пуст");
+        }
+        foreach (var user in users)
+        {
+          Console.WriteLine(user.ToString());
+        }
+      }catch (Exception ex)
+      {
+        Console.WriteLine($"Ошибка: {ex.Message}");
+      }
+    }
+  }
+}
---
+++ UserManagement/User.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserManagement
+{
+  public class User
+  {
+    /// <summary>
+    /// Имя пользователя
+    /// </summary>
+    public string Name { get; set; }
+    /// <summary>
+    /// Возраст пользователя
+    /// </summary>
+    public decimal Age { get; set; }
+
+    public User(string name, int age)
+    {
+      Name = name;
+      Age = age;
+    }
+
+    public override string ToString()
+    {
+      return $"Name: {Name}, Age: {Age}";
+    }
+  }
+}
